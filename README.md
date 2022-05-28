2d1356d2ca5a1f60a05252b4fc932bbe596c6988
UP-4303 <up4303.pro@protonmail.com>
2022-05-28 18:10:08 +0200

    Dernière MAJ du jour
    Commentaires, possibilité de choisir la configuration de départ

--- README.md
+++ README.md
@@ -8,16 +8,17 @@ Une fois que cela est fait, vous pouvez lancer le script "main.py" depuis votre 
 * 2 => Hauteur d'une génération. Le paramètre est ignoré si l'automate n'a qu'une dimension. Entier positif
 * 3 => Nombre de générations. Entier
 * 4 => Conserver toutes les générations. 1 si vous souhaitez conserver une image toutes les générations, 0 sinon. Le paramètre est ignoré si l'automate n'a qu'une dimension. 0 ou 1
-* 5 => Nombre d'états possibles. Par défaut 2. Entier positif
+* 5 => Génération initiale aléatoire. 0 utilisera la génération dans "start.txt", complétée par des 0. 0 ou 1
+* 6 => Nombre d'états possibles. Entier positif
 
 Pour l'écriture de vos règles :
 * La génération précédente est la liste "gen".
 * La cellule sur laquelle est appliquée la règle est en coordonnée [i] ou [i][j] si vous utilisez 2 dimensions.
 * Notez que par simplicité technique l'espace utilisé n'est pas Z^d mais un tore dont vous définissez les dimensions dans les arguments. Pour un automate à une dimension, si vous utilisez la valeur d'une cellule avec une coordonnée superieur à i veuillez noter [(i+x)%size[0]]. Pour un automate à deux dimensions, si vous utilisez la valeur d'une cellule avec des coordonnées superieur à i ou j, veuillez noter [(i+x)%size[1]] et/ou [(j+y)%size[0]].
-* Les états des cellules vont de 0 à (args[5]-1).
+* Les états des cellules vont de 0 à (args[6]-1).
 
 Quelques exemples de règles :
 * Le jeu de la vie, 2D : (gen[i][j] and (gen[i-1][j-1]+gen[i-1][j]+gen[i-1][(j+1)%size[0]]+gen[i][j-1]+gen[i][(j+1)%size[0]]+gen[(i+1)%size[1]][j-1]+gen[(i+1)%size[1]][j]+gen[(i+1)%size[1]][(j+1)%size[0]] in [2,3])) or (not(gen[i][j]) and (gen[i-1][j-1]+gen[i-1][j]+gen[i-1][(j+1)%size[0]]+gen[i][j-1]+gen[i][(j+1)%size[0]]+gen[(i+1)%size[1]][j-1]+gen[(i+1)%size[1]][j]+gen[(i+1)%size[1]][(j+1)%size[0]] == 3))
 * Règle élémentaire 110, 1D : (gen[(i+1)%size[0]] and (not(gen[i-1]) or not(gen[i]))) or (gen[i] and not(gen[(i+1)%size[0]]))
 * Règle élémentaire 30, 1D : gen[i-1]^(gen[i]|gen[(i+1)%size[0]])
-* Automate cyclique de D. Griffeath, 2D : (gen[i][j] + [1 if len([x for x in [gen[i-1][j-1],gen[i-1][j],gen[i-1][(j+1)%size[0]],gen[i][j-1],gen[i][(j+1)%size[0]],gen[(i+1)%size[1]][j-1],gen[(i+1)%size[1]][j],gen[(i+1)%size[1]][(j+1)%size[0]]] if x == (gen[i][j]+1)%(args[5])]) >= 3 else 0][0]) % (args[5])
+* Automate cyclique de D. Griffeath, 2D : (gen[i][j] + [1 if len([x for x in [gen[i-1][j-1],gen[i-1][j],gen[i-1][(j+1)%size[0]],gen[i][j-1],gen[i][(j+1)%size[0]],gen[(i+1)%size[1]][j-1],gen[(i+1)%size[1]][j],gen[(i+1)%size[1]][(j+1)%size[0]]] if x == (gen[i][j]+1)%(args[6])]) >= 3 else 0][0]) % (args[6])
--- main.py
+++ main.py
@@ -2,83 +2,138 @@ import sys
 from PIL import Image
 from random import randint
 from datetime import datetime
-import imageio
+import imageio.v2 as imageio
 
-def time():
+def timeFunc():
+    """
+    Donne la date et l'heure actuelles, formatées pour être utilisées comme nom de fichier.
+    """
     return datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
-try:
+try: # Récupération des valeurs passées dans la commande. Si un argument est invalide une erreur est levée
     args = [int(sys.argv[i]) for i in (1,2,3,4)]
 
     assert sys.argv[5] == "0" or sys.argv[5] == "1"
+    assert sys.argv[6] == "0" or sys.argv[6] == "1"
 
     args.append(bool(int(sys.argv[5])))
-    try:
-        args.append(int(sys.argv[6]))
-    except:
-        args.append(2)
+    args.append(bool(int(sys.argv[6])))
+    args.append(int(sys.argv[7]))
 
     assert args[0] == 1 or args[0] == 2
 except:
     raise ValueError("/!\ Paramètres incorrects.")
 
-if args[0] == 1:
-    size = (args[1], args[3])
-    gen = [randint(0, args[5]-1) for i in range(size[0])]
-    im = Image.new(mode="RGB", size=size)
+if args[0] == 1: # Si l'automate est en 1D
+    size = (args[1], args[3]) # Largeur, hauteur
+    
+    if args[5]: # Si on utilise une génération initiale aléatoire
+        gen = [randint(0, args[6]-1) for i in range(size[0])]
+    else:
+        with open("start.txt", 'r') as f:
+            gen = f.readlines()
+        
+        if len(gen) > 1 or len(gen[0]) > size[0]: # Si le contenu du fichier est trop grand, une erreur est levée
+            raise ValueError("/!\ Génération de départ invalide.")
+        else:
+            right = True
+            gen = list(gen[0]) # str -> list[str]
+            gen = [int(i) for i in gen] # str -> int
+            while len(gen) < size[0]: # On complète avec des 0
+                if right:
+                    gen.append(0)
+                else:
+                    gen.insert(0,0)
+                right = not right # Alternance gauche/droite
 
-    with open("rule.txt", 'r') as f:
+    im = Image.new(mode="RGB", size=size) # Création de l'image avec le module PIL
+
+    with open("rule.txt", 'r') as f: # Récupération de la règle
         rule = f.read()
 
-    for i in range(size[0]):
-        color = (255//(args[5]-1)) * (-gen[i]+(args[5]-1))
+    for i in range(size[0]): # On imprime la première génération en haut de l'image
+        color = (255//(args[6]-1)) * (-gen[i]+(args[6]-1))
         im.putpixel((i,0),(color,color,color))
 
     for turn in range(1, size[1]):
-        newLine = []
+        newLine = [] # Génération
         for i in range(size[0]):
-            pixel = eval(rule)
+            pixel = eval(rule) # On évalue la règle avec les valeurs actuelles pour chaque cellule
             newLine.append(pixel)
-            color = (255//(args[5]-1)) * (-pixel+(args[5]-1))
+            color = (255//(args[6]-1)) * (-pixel+(args[6]-1))
             im.putpixel((i,turn),(color,color,color))
-        gen = newLine
     
-    im.save(f"images/{time()}.jpg", "JPEG")
+    im.save(f"images/{timeFunc()}.jpg", "JPEG") # On enregistre l'image
 
-else:
+else: # Si l'automate est en 2D
     size = (args[1], args[2])
-    gen = [[randint(0, args[5]-1) for j in range(size[0])] for i in range(size[1])]
-    im = Image.new(mode="RGB", size=size)
 
-    with open("rule2.txt", 'r') as f:
+    if args[5]: # Si on utilise une génération initiale aléatoire
+        gen = [[randint(0, args[6]-1) for j in range(size[0])] for i in range(size[1])]
+    else:
+        with open("start.txt", 'r') as f:
+            gen = f.readlines()
+        
+        for i in range(len(gen)): # Conversion en list[list[int]]
+            if i == len(gen)-1:
+                gen[i] = list(gen[i])
+            else:
+                gen[i] = list(gen[i][:-1])
+            if len(gen[i]) > size[0]: # Si la génération donnée est trop grande on lève une erreur
+                raise ValueError("/!\ Génération de départ invalide.")
+        if len(gen) > size[1]:
+            raise ValueError("/!\ Génération de départ invalide.")
+
+        down = True
+        gen = [[int(j) for j in i] for i in gen]
+        while len(gen) < size[1]: # On complète avec des lignes de 0
+            if down:
+                gen.append([0 for i in range(size[0])])
+            else:
+                gen.insert(0, [0 for i in range(size[0])])
+            down = not down # Alternance haut/bas
+        for i in range(len(gen)):
+            right = True
+            while len(gen[i]) < size[0]: # On complète la ligne avec des 0
+                if right:
+                    gen[i].append(0)
+                else:
+                    gen[i].insert(0,0)
+                right = not right # Alternance gauche/droite
+
+
+    im = Image.new(mode="RGB", size=size) # Création de l'image avec le module PIL
+
+    with open("rule2.txt", 'r') as f: # Récupération de la règle
         rule = f.read()
     
-    for i in range(size[1]):
+    for i in range(size[1]): # Première image, correspondant à la génération initiale
         for j in range(size[0]):
-            color = (255//(args[5]-1)) * (-gen[i][j]+(args[5]-1))
+            color = (255//(args[6]-1)) * (-gen[i][j]+(args[6]-1))
             im.putpixel((j,i),(color,color,color))
 
-    time = time()
+    time = timeFunc() # Heure de début
     filenames = [f"images/{time}--0.jpg"]
-    im.save(filenames[0], "JPEG")
+    im.save(filenames[0], "JPEG") # On enregistre la première image
 
     for turn in range(1, args[3]):
         newGen = []
         for i in range(size[1]):
             newLine = []
             for j in range(size[0]):
-                pixel = eval(rule)
+                pixel = eval(rule) # Evaluation de la règle avec les valeurs actuelles pour chaque cellule
                 newLine.append(pixel)
-                color = (255//(args[5]-1)) * (-pixel+(args[5]-1))
+                color = (255//(args[6]-1)) * (-pixel+(args[6]-1))
                 im.putpixel((j,i),(color,color,color))
             newGen.append(newLine)
         gen = newGen
-        if args[4] or turn == args[3]-1:
+        if args[4] or turn == args[3]-1: # Si on enregistre chaque génération ou si c'est la dernière génération
             filenames.append(f"images/{time}--{str(turn)}.jpg")
             im.save(filenames[turn], "JPEG")
-    if args[4]:
+    if args[4]: # Si on enregistre chaque génération
         images = []
         for filename in filenames:
             images.append(imageio.imread(filename))
-        imageio.mimsave(f'images/{time}.gif', images)
-    print(f"Terminé ! Fin de l'execution : {time()}")
+        imageio.mimsave(f'images/{time}.gif', images) # On fait un gif
+
+print(f"Terminé ! Fin de l'exécution : {timeFunc()}") # On indique l'heure de fin à l'utilisateur, pour qu'il puisse connaître le temps d'exécution
